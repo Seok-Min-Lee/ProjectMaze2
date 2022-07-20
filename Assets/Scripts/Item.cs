90705b7479dad7f5a07f00e46fe5e78b678fde96
Seok-Min-Lee <57294634+Seok-Min-Lee@users.noreply.github.com>
2022-07-20 15:29:28 +0900

    Player의 Item 획득 기능 추가.

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -7,10 +7,12 @@ public class Item : MonoBehaviour
     public ItemType type;
     public int value;
 
+    public Transform mesh;
+
     float rotateSpeed = 0.3f;
 
     void Update()
     {
-        transform.Rotate(axis: Vector3.up, angle: rotateSpeed);
+        mesh.Rotate(axis: Vector3.up, angle: rotateSpeed);
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform respawnPosition;
+    public int currentHp, maxHp;
+    public int currentLife, maxLife;
+    public bool enableMinimap;
+    public bool[] enableBeads;
+
+    private void Awake()
+    {
+        enableBeads = new bool[3];
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Item")
+        {
+            GameObject gameObject = other.gameObject;
+
+            GetItem(gameObject: gameObject);
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void GetItem(GameObject gameObject)
+    {
+        Item item = gameObject.GetComponent<Item>();
+
+        switch (item.type)
+        {
+            case ItemType.Heal:
+                ChangeCurrentHp(value: item.value);
+                break;
+            case ItemType.Map:
+                ActivateMinimap();
+                break;
+            case ItemType.Bead:
+                ActivateBead(index: item.value);
+                break;
+        }
+    }
+
+    private void ChangeCurrentHp(int value)
+    {
+        currentHp += value;
+
+        if (currentHp > maxHp)
+        {
+            currentHp = maxHp;
+        }
+        else if (currentHp < 0)
+        {
+            currentHp = 0;
+        }
+    }
+
+    private void ActivateMinimap()
+    {
+        enableMinimap = true;
+    }
+
+    private void ActivateBead(int index)
+    {
+        if(index > 0 && index < enableBeads.Length)
+        {
+            enableBeads[index] = true;
+        }
+    }
+}
