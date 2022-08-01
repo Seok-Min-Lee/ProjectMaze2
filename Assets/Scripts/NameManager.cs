bc3127b4016bab3fa2ed53108071eaa3d125fea5
Seok-Min-Lee <57294634+Seok-Min-Lee@users.noreply.github.com>
2022-08-01 22:53:16 +0900

    NPC 구현하기 위한 NameManager 업데이트.
    태그 및 NPC명 추가.

--- Assets/Scripts/NameManager.cs
+++ Assets/Scripts/NameManager.cs
@@ -10,6 +10,9 @@ public static class NameManager
     public const string TAG_MONSTER_TURN_BACK_AREA= "MonsterTurnBackArea";
     public const string TAG_TRAP_ACTIVATOR = "TrapActivator";
     public const string TAG_TRAP_DEACTIVATOR = "TrapDeactivator";
+    public const string TAG_TRAP = "Trap";
+    public const string TAG_NPC = "NPC";
+    public const string TAG_NPC_INTERACTION_ZONE = "NpcInteractionZone";
 
     public const string LAYER_PLAYER = "Player";
 
@@ -17,4 +20,14 @@ public static class NameManager
     public const string ANIMATION_PARAMETER_STAB_ATTACK = "Stab Attack";
     public const string ANIMATION_PARAMETER_DO_ATTACK = "DoAttack";
     public const string ANIMATION_PARAMETER_DO_DIE = "DoDie";
+
+    public const string NPC_NAME_HUMAN_GATEKEEPER = "문지기";
+    public const string NPC_NAME_FAIRY_CHILD = "요정 아이";
+    public const string NPC_NAME_FAIRY_OLD_MAN = "요정 장로";
+    public const string NPC_NAME_GIANT_STONE_STATUE = "거인 석상";
+    public const string NPC_NAME_GIANT_TWIN_A = "쌍둥이 거인 A";
+    public const string NPC_NAME_GIANT_TWIN_B = "쌍둥이 거인 B";
+    public const string NPC_NAME_HUMAN_YOUNG_MAN = "인간 청년";
+    public const string NPC_NAME_HUMAN_OLD_MAN = "인간 노인";
+    public const string NPC_NAME_NONAME = "인게임 NPC";
 }
