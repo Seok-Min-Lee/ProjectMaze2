524127b28603210c9a83dee39a38137aca444c14
Seok-Min-Lee <57294634+Seok-Min-Lee@users.noreply.github.com>
2022-08-19 03:38:23 +0900

    Item 스크립트에서 구슬 아이템 색 설정하도록 업데이트.

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -11,8 +11,31 @@ public class Item : MonoBehaviour
 
     float rotateSpeed = 0.3f;
 
-    void Update()
+    private void Start()
     {
-        mesh.Rotate(axis: Vector3.up, angle: rotateSpeed);
+        if(type == ItemType.Bead)
+        {
+            Color color = Color.white;
+
+            switch (value)
+            {
+                case 0:
+                    color = Color.red;
+                    break;
+                case 1:
+                    color = Color.green;
+                    break;
+                case 2:
+                    color = Color.blue;
+                    break;
+            }
+
+            mesh.GetComponentInChildren<Renderer>().material.color = color;
+        }
+    }
+
+    private void Update()
+    {
+        mesh.Rotate(axis: Vector3.up, angle: rotateSpeed * Time.deltaTime);
     }
 }
