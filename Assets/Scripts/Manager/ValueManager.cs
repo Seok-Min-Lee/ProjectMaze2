760b909277dbd9b6f024ae97eb5e8686b7d608b5
Seok-Min-Lee <57294634+Seok-Min-Lee@users.noreply.github.com>
2022-08-24 17:57:00 +0900

    플레이어 이동속도 업데이트.

--- Assets/Scripts/Manager/ValueManager.cs
+++ Assets/Scripts/Manager/ValueManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public static class ValueManager
 {
     public const float PLAYER_MOVE_SPEED_DEFAULT = 4f;  // 플레이어 이동 속도 기본값
-    public const float PLAYER_SPRINT_SPEED_DEFAULT = 20f;   // 플레이어 달리기 속도 기본값
+    public const float PLAYER_SPRINT_SPEED_DEFAULT = 10f;   // 플레이어 달리기 속도 기본값
     public const float PLAYER_DETOX_ACTIVATE_TIME = 5.0f;     //'중독' 상태 해제를 위한 미입력 시간
     public const float PLAYER_CONFUSION_STACK_UPDATE_TIME = 1.0f;   //'공포' 스택 업데이트 시간
     public const float PLAYER_CONFUSION_DURATION = 5.0f;  // '공포' 발현 시 지속 시간
