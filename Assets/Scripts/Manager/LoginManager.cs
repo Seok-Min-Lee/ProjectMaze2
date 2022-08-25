67a7578ec11e44926dcfc7c603b6bb063edbab06
Seok-Min-Lee <57294634+Seok-Min-Lee@users.noreply.github.com>
2022-08-25 21:16:17 +0900

    로그인 시작 시 에러 메세지 출력 되는 오류 수정.

--- Assets/Scripts/Manager/LoginManager.cs
+++ Assets/Scripts/Manager/LoginManager.cs
@@ -6,20 +6,18 @@ using UnityEngine.UI;
 
 public class LoginManager : MonoBehaviour
 {
-    const string LOGIN_FAILURE_MESSAGE = "로그인 실패. 로그인 정보를 확인해주시길 바랍니다.";
-    const string MODE_FAILURE_MESSAGE = "이어하기 실패. 저장된 게임 정보가 없습니다.";
+    const string ERROR_MESSAGE_LOGIN_FAIL = "로그인 실패. 로그인 정보를 확인해주시길 바랍니다.";
+    const string ERROR_MESSAGE_MODE_SELECT_FAIL = "이어하기 실패. 저장된 게임 정보가 없습니다.";
 
     public GameObject inputPanel, modePanel;
     public Text inputAccount, inputPassword, failureText;
-
-    private void Awake()
-    {
-    }
 
     private void Start()
     {
         inputPanel.SetActive(true);
         modePanel.SetActive(false);
+
+        failureText.text = string.Empty;
     }
 
     public void OnClickExit()
@@ -41,7 +39,7 @@ public class LoginManager : MonoBehaviour
         else
         {
             StopCoroutine(AlertMessage(text: string.Empty));
-            StartCoroutine(AlertMessage(text: LOGIN_FAILURE_MESSAGE));
+            StartCoroutine(AlertMessage(text: ERROR_MESSAGE_LOGIN_FAIL));
         }
     }
 
@@ -60,7 +58,7 @@ public class LoginManager : MonoBehaviour
         else
         {
             StopCoroutine(AlertMessage(text: string.Empty));
-            StartCoroutine(AlertMessage(text: MODE_FAILURE_MESSAGE));
+            StartCoroutine(AlertMessage(text: ERROR_MESSAGE_MODE_SELECT_FAIL));
         }
     }
 
